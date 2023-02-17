9c9d292e5a5d9d9c24957fb63f89a3ccd0924115
Amir Giveon <agiveon@gmail.com>
2023-02-17 06:47:13 -0800

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from google.oauth2 import service_account
+import gspread
+import datetime
+import requests
+import pickle
+
+st.write('hello world')

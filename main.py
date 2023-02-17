7e36445f5f7789a1ea6d0b155883cc8c1c8bc2f1
Amir Giveon <agiveon@gmail.com>
2023-02-17 07:36:31 -0800

    test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from google.oauth2 import service_account
 import random
 
+numbers = random.sample(range(1, 7), 6)
 
 CARD_WIDTH = 100
 
@@ -11,15 +12,15 @@ st.subheader('Use all these cards:')
 col1, col2, col3, col4, col5 = st.columns(5)
 # Add a card to each column
 with col1:
-    st.image("cards/1.jpg", width=CARD_WIDTH)
+    st.image(f"cards/{numbers[0]}.jpg", width=CARD_WIDTH)
 with col2:
-    st.image("cards/2.jpg", width=CARD_WIDTH)
+    st.image(f"cards/{numbers[1]}.jpg", width=CARD_WIDTH)
 with col3:
-    st.image("cards/3.jpg", width=CARD_WIDTH)
+    st.image(f"cards/{numbers[2]}.jpg", width=CARD_WIDTH)
 with col4:
-    st.image("cards/4.jpg", width=CARD_WIDTH)
+    st.image(f"cards/{numbers[3]}.jpg", width=CARD_WIDTH)
 with col5:
-    st.image("cards/5.jpg", width=CARD_WIDTH)
+    st.image(f"cards/{numbers[4]}.jpg", width=CARD_WIDTH)
 
 
 st.write("")
@@ -28,5 +29,5 @@ st.subheader('to reach:')
 # Add a card to each column
 col1, col2, col3, col4, col5 = st.columns(5)
 with col3:
-    st.image("cards/6.jpg", width=CARD_WIDTH)
+    st.image(f"cards/{numbers[5]}.jpg", width=CARD_WIDTH)
 

9b50cfbe5917e20fe69f612702916516587c019c
Amir Giveon <agiveon@gmail.com>
2023-02-17 06:57:26 -0800

    test

--- main.py
+++ main.py
@@ -14,15 +14,14 @@ col1, col2, col3, col4, col5 = st.beta_columns(5)
 # Add a card to each column
 with col1:
     st.image("cards/1.jpg", width=CARD_WIDTH)
-
 with col2:
     st.image("cards/1.jpg", width=CARD_WIDTH)
-
 with col3:
     st.image("cards/1.jpg", width=CARD_WIDTH)
-
 with col4:
     st.image("cards/1.jpg", width=CARD_WIDTH)
-
 with col5:
     st.image("cards/1.jpg", width=CARD_WIDTH)
+
+st.title('Goal:')
+st.image("cards/1.jpg", width=CARD_WIDTH)

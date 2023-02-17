2190446111309aa435417d2585ec4f369e17d77e
Amir Giveon <agiveon@gmail.com>
2023-02-17 06:54:56 -0800

    cards

--- main.py
+++ main.py
@@ -7,4 +7,27 @@ import datetime
 import requests
 import pickle
 
-st.write('hello world')
+CARD_WIDTH = 200
+
+col1, col2, col3, col4, col5 = st.beta_columns(5)
+
+# Add a card to each column
+with col1:
+    st.title("Card 1")
+    st.image("cards/1.jpg", width=CARD_WIDTH)
+
+with col2:
+    st.title("Card 2")
+    st.image("cards/1.jpg", width=CARD_WIDTH)
+
+with col3:
+    st.title("Card 3")
+    st.image("cards/1.jpg", width=CARD_WIDTH)
+
+with col4:
+    st.title("Card 4")
+    st.image("cards/1.jpg", width=CARD_WIDTH)
+
+with col5:
+    st.title("Card 5")
+    st.image("cards/1.jpg", width=CARD_WIDTH)

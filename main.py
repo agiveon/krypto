b59371c8265ad60a106442755deb0f50d08bd5b0
Amir Giveon <agiveon@gmail.com>
2023-02-17 07:34:49 -0800

    test

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from google.oauth2 import service_account
-import gspread
-import datetime
-import requests
-import pickle
+import random
+
 
 CARD_WIDTH = 100
 

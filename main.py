7563abbe1114e8d18ba90d19c35c86b46f4097ef
Amir Giveon <agiveon@gmail.com>
2023-02-17 09:16:35 -0800

    test

--- main.py
+++ main.py
@@ -56,8 +56,6 @@ if "button1" not in st.session_state:
 if "button2" not in st.session_state:
     st.session_state["button2"] = False
 
-if "button3" not in st.session_state:
-    st.session_state["button3"] = False
 
 if st.button("Button1"):
     st.session_state["button1"] = not st.session_state["button1"]
@@ -66,13 +64,8 @@ if st.session_state["button1"]:
     if st.button("Button2"):
         st.session_state["button2"] = not st.session_state["button2"]
 
-if st.session_state["button1"] and st.session_state["button2"]:
-    if st.button("Button3"):
-        # toggle button3 session state
-        st.session_state["button3"] = not st.session_state["button3"]
-
-if st.session_state["button3"]:
-    st.write("**Button3!!!**")
+if st.session_state["button2"]:
+    st.write("**Button2!!!**")
 
 
 # Print the session state to make it easier to see what's happening
@@ -83,7 +76,6 @@ st.write(
 
     {st.session_state["button2"]=}
 
-    {st.session_state["button3"]=}
     """
 )
 
